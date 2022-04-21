ee2efb341e8bc4a3159a31e8adc754a13f1e92ea
CJ <ccrju19@gmail.com>
2022-04-20 22:09:54 -0400

    added date component to main page

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { Grid, Paper, Container } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Container,
+  Typography,
+  DialogTitle,
+  Divider,
+  Box,
+} from "@mui/material";
 import LineGraph from "./LineGraph";
 import PieChart from "./PieChart";
+import CalendarMonthTwoToneIcon from "@mui/icons-material/CalendarMonthTwoTone";
 
 const MainPage = () => {
+  const month = new Date().toLocaleString("default", { month: "long" });
+  const day = new Date().getDate();
+  const year = new Date().getFullYear();
+  const dayOfWeek = new Date().toLocaleString("default", { weekday: "long" });
+
   return (
     <>
       <h1>Main</h1>
@@ -14,13 +28,61 @@ const MainPage = () => {
             <Paper
               elevation={3}
               sx={{
-                p: 5,
-                display: "flex",
-                flexDirection: "column",
                 height: 200,
               }}
             >
-              Today's Date
+              <Grid container>
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    sx={{ backgroundColor: "#001D3A", color: "lightgrey" }}
+                  >
+                    <DialogTitle>
+                      <Typography sx={{ fontSize: 16 }}>
+                        <CalendarMonthTwoToneIcon
+                          sx={{ marginRight: 2, marginBottom: -0.5 }}
+                        />
+                        {dayOfWeek}
+                      </Typography>
+                    </DialogTitle>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Grid container>
+                    <Grid
+                      item
+                      xs={5}
+                      container
+                      justifyContent="flex-end"
+                      mt={2}
+                    >
+                      <Typography
+                        sx={{ fontSize: 16 }}
+                        color="text.secondary"
+                        variant="overline"
+                        // mr={2}
+                      >
+                        {month}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={7} container justifyContent="flex-start">
+                      <Typography color="text.secondary" sx={{ fontSize: 75 }}>
+                        {day}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Box justifyContent="center" mt={-0.75}>
+                <Typography
+                  sx={{ letterSpacing: 3 }}
+                  color="text.secondary"
+                  variant="h6"
+                >
+                  <Divider> {year}</Divider>
+                </Typography>
+              </Box>
             </Paper>
           </Grid>
 
