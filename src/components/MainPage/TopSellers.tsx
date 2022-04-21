6708a913bdc0cbf205f85c3b59a6d81b620fcf1d
CJ <ccrju19@gmail.com>
2022-04-21 14:23:45 -0400

    added top sellers component to main page

--- src/components/MainPage/TopSellers.tsx
+++ src/components/MainPage/TopSellers.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Grid, Typography, DialogTitle, Divider, Box } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+} from "@mui/material";
 import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
 
 const TopSellers = () => {
@@ -10,7 +18,7 @@ const TopSellers = () => {
     axios
       .get("api/v1/dashboard/pie")
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data);
         setTopSellers(res.data);
       })
       .catch((err) => {
@@ -41,7 +49,55 @@ const TopSellers = () => {
         </Grid>
         <Grid item xs={12}>
           <Grid container justifyContent="center">
-            content
+            <List sx={{ marginTop: 1.5, marginLeft: 1, padding: 0 }}>
+              <ListItem sx={{ padding: 0.5 }}>
+                <Avatar
+                  sx={{
+                    width: 25,
+                    height: 25,
+                    fontSize: 30,
+                    bgcolor: "#6D006D",
+                  }}
+                >
+                  1
+                </Avatar>
+                <Typography color="#3A001D" variant="body2" sx={{marginLeft: 1, letterSpacing: 2}}>
+                  {Object.keys(topSellers)[0]}
+                </Typography>
+              </ListItem>
+
+              <ListItem sx={{ padding: 0.5 }}>
+                <Avatar
+                  sx={{
+                    width: 25,
+                    height: 25,
+                    fontSize: 25,
+                    bgcolor: "#6D006D",
+                  }}
+                >
+                  2
+                </Avatar>
+                <Typography color="#3A001D" variant="body2" sx={{marginLeft: 1, letterSpacing: 2}}>
+                  {Object.keys(topSellers)[1]}
+                </Typography>
+              </ListItem>
+
+              <ListItem sx={{ padding: 0.5 }}>
+                <Avatar
+                  sx={{
+                    width: 25,
+                    height: 25,
+                    fontSize: 21,
+                    bgcolor: "#6D006D",
+                  }}
+                >
+                  3
+                </Avatar>
+                <Typography color="#3A001D" variant="body2" sx={{marginLeft: 1, letterSpacing: 2}}>
+                  {Object.keys(topSellers)[2]}
+                </Typography>
+              </ListItem>
+            </List>
           </Grid>
         </Grid>
       </Grid>
