c883e012c41cf2414cc80d3501d3b0a15a76f8df
CJ <ccrju19@gmail.com>
2022-06-08 13:48:59 -0400

    changed display content for prodpopover component

--- src/components/Products/ProdPopover.tsx
+++ src/components/Products/ProdPopover.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React from "react";
 import { IPopover as Props } from "./List";
 import { Grid, Typography, Popover, Box, Button } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
@@ -12,7 +12,6 @@ const ProdPopover: React.FC<Props> = ({
   handleCloseDeleteProd,
   popoverProd,
 }) => {
-    
   return (
     <>
       <Popover
@@ -29,15 +28,22 @@ const ProdPopover: React.FC<Props> = ({
           horizontal: "left",
         }}
       >
-        <Typography variant="body1" sx={{ p: 2 }} color="primary">
+        <Typography variant="body1" sx={{ p: 2 }} color="#01579b">
           <InfoIcon sx={{ marginBottom: -0.5, marginRight: 1 }} />
           Item can only be deleted from the database if no existing orders
-          contain the Item (for historical data logging)
+          contain the item, for historical data logging
         </Typography>
-        <Box display="flex" justifyContent="center" mb={2}>
+        <Box display="flex" justifyContent="center" mb={2} ml={2} mr={2}>
           <Typography variant="caption" color="text.secondary">
-            Mark the item as inactive to "soft delete", if it cannot be
-            deleted or is attached to orders
+            Mark the item as inactive to "soft delete", if it cannot be deleted
+            (attached to orders)
+          </Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="center" mb={2} ml={2} mr={2}>
+          <Typography variant="caption" color="text.secondary">
+            ** Note: Inactive status is currently for products only - feature
+            implementation for product options in progress
           </Typography>
         </Box>
 
